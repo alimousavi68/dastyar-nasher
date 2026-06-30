79ee0a7a585e0d315be29db46950def435b08ce8
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2026-06-30 16:27:00 +0330

    fix

--- admin-pages/feeds_list.php
+++ admin-pages/feeds_list.php
@@ -434,17 +434,17 @@ function custom_rss_parser_display_items()
                         </div>
 
                         <!-- Left Side: Actions -->
-                        <div class="action-bar d-flex align-items-center gap-2" <?php if($feed_status === 'published') echo 'style="opacity: 0.5; pointer-events: none;"'; ?>>
+                        <div class="action-bar d-flex align-items-center gap-2">
                             <!-- Priority Selector (Without Tooltips) -->
-                            <div class="priority-selector-group" <?php if($feed_status === 'published') echo 'style="background: transparent; border-color: transparent;"'; ?>>
+                            <div class="priority-selector-group">
                                 <span class="priority-label">اولویت صف</span>
-                                <button type="button" class="scrape-link btn-priority high" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="high" data-resource-id="<?php echo $resource_id; ?>" <?php if($feed_status === 'published') echo 'disabled'; ?>>بالا</button>
-                                <button type="button" class="scrape-link btn-priority medium" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="medium" data-resource-id="<?php echo $resource_id; ?>" <?php if($feed_status === 'published') echo 'disabled'; ?>>متوسط</button>
-                                <button type="button" class="scrape-link btn-priority low" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="low" data-resource-id="<?php echo $resource_id; ?>" <?php if($feed_status === 'published') echo 'disabled'; ?>>کم</button>
+                                <button type="button" class="scrape-link btn-priority high" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="high" data-resource-id="<?php echo $resource_id; ?>">بالا</button>
+                                <button type="button" class="scrape-link btn-priority medium" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="medium" data-resource-id="<?php echo $resource_id; ?>">متوسط</button>
+                                <button type="button" class="scrape-link btn-priority low" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="low" data-resource-id="<?php echo $resource_id; ?>">کم</button>
                             </div>
 
                             <!-- Fetch Custom Button -->
-                            <button type="button" class="scrape-link btn-fetch-custom position-relative" data-bs-toggle="tooltip" data-bs-title="واکشی مستقیم" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="pending" data-resource-id="<?php echo $resource_id; ?>" <?php if($feed_status === 'published') echo 'disabled'; ?>>
+                            <button type="button" class="scrape-link btn-fetch-custom position-relative" data-bs-toggle="tooltip" data-bs-title="واکشی مستقیم" data-id="<?php echo $item->id; ?>" data-guid="<?php echo $item_guid; ?>" data-priority="pending" data-resource-id="<?php echo $resource_id; ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-cloud-download" viewBox="0 0 16 16">
                                     <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>
                                     <path d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708z"/>
@@ -453,13 +453,13 @@ function custom_rss_parser_display_items()
                             </button>
 
                             <!-- Publish Now Button -->
-                            <a href="javascript:void(0);" class="btn-action-yellow scrape-link position-relative" data-guid="<?php echo $item_guid; ?>" data-resource-id="<?php echo $resource_id; ?>" data-id="<?php echo $item_id; ?>" data-priority="now" data-bs-toggle="tooltip" data-bs-title="انتشار فوری" <?php if($feed_status === 'published') echo 'disabled style="pointer-events:none;"'; ?>>
+                            <a href="javascript:void(0);" class="btn-action-yellow scrape-link position-relative" data-guid="<?php echo $item_guid; ?>" data-resource-id="<?php echo $resource_id; ?>" data-id="<?php echo $item_id; ?>" data-priority="now" data-bs-toggle="tooltip" data-bs-title="انتشار فوری">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-lightning-charge" viewBox="0 0 16 16"><path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41z"/></svg>
                                 <img src="<?php echo $admin_url; ?>" class="i8-loader-gif" style="display:none;position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);z-index:100;width:20px;" />
                             </a>
 
                             <!-- View Original Button -->
-                            <a href="<?php echo $item_guid; ?>" target="_blank" class="btn-action-gray" data-bs-toggle="tooltip" data-bs-title="مشاهده اصلی" <?php if($feed_status === 'published') echo 'disabled style="pointer-events:none;"'; ?>>
+                            <a href="<?php echo $item_guid; ?>" target="_blank" class="btn-action-gray" data-bs-toggle="tooltip" data-bs-title="مشاهده اصلی">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
@@ -467,7 +467,7 @@ function custom_rss_parser_display_items()
                             </a>
 
                             <!-- Delete Button -->
-                            <a href="<?php echo esc_url(wp_nonce_url(add_query_arg(array('action' => 'delete_item', 'item_id' => $item_id)), 'cop_delete_item_nonce')); ?>" class="btn-action-danger delete-item-link" data-bs-toggle="tooltip" data-bs-title="حذف فید" <?php if($feed_status === 'published') echo 'disabled style="pointer-events:none;"'; ?>>
+                            <a href="<?php echo esc_url(wp_nonce_url(add_query_arg(array('action' => 'delete_item', 'item_id' => $item_id)), 'cop_delete_item_nonce')); ?>" class="btn-action-danger delete-item-link" data-bs-toggle="tooltip" data-bs-title="حذف فید">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                                 </svg>
@@ -755,12 +755,6 @@ function custom_rss_parser_display_items()
                                                     badgeWrapper.innerHTML = newBadgeHtml;
                                                 }
                                                 
-                                                if (publish_priority === "now") {
-                                                    $(rowEl).find('.action-bar').css({
-                                                        'opacity': '0.5',
-                                                        'pointer-events': 'none'
-                                                    });
-                                                }
                                             }
                                         } else {
                                             showCustomToast(res.message, 'error', postTitle);
